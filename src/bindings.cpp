ca68ce129bd1b06203c8d8073faf3c61ad4fb28c
endail <52652357+endail@users.noreply.github.com>
2021-08-20 23:53:02 +1000

    more changes

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -14,6 +14,50 @@ PYBIND11_MODULE(HX711, m) {
     m.doc() = "HX711 namespace";
 
     /**
+     * HX711.Channel
+     */
+    py::enum_<HX711::Channel>(m, "Channel")
+        .value("A", HX711::Channel::A)
+        .value("B", HX711::Channel::B);
+
+    /**
+     * HX711.Gain
+     */
+    py::enum_<HX711::Gain>(m, "Gain")
+        .value("GAIN_128", HX711::Gain::GAIN_128)
+        .value("GAIN_32", HX711::Gain::GAIN_32)
+        .value("GAIN_64", HX711::Gain::GAIN_64);
+
+    /**
+     * HX711.Rate
+     */
+    py::enum_<HX711::Rate>(m, "Rate")
+        .value("HZ_10", HX711::Rate::HZ_10)
+        .value("HZ_80", HX711::Rate::HZ_80)
+        .value("OTHER", HX711::Rate::OTHER);
+
+    /**
+     * HX711.Format
+     */
+    py::enum_<HX711::Format>(m, "Format")
+        .value("MSB", HX711::Format::MSB)
+        .value("LSB", HX711::Format::LSB);
+
+    /**
+     * HX711.StrategyType
+     */
+    py::enum_<HX711::StrategyType>(m, "StrategyType")
+        .value("Samples", StrategyType::Samples)
+        .value("Time", StrategyType::Time);
+
+    /**
+     * HX711.ReadType
+     */
+    py::enum_<HX711::ReadType>(m, "ReadType")
+        .value("Median", ReadType::Median)
+        .value("Average",ReadType::Average);
+
+    /**
      * HX711.GpioException
      */
     py::class_<GpioException>(m, "GpioException")
@@ -37,60 +81,62 @@ PYBIND11_MODULE(HX711, m) {
      */
     py::class_<Mass> mass_Type(m, "Mass");
 
-    mass_Type.def_static("convert", &Mass::convert);
-
     py::enum_<Mass::Unit>(mass_Type, "Unit")
-      .value("UG", Mass::Unit::UG)
-      .value("MG", Mass::Unit::MG)
-      .value("G", Mass::Unit::G)
-      .value("KG", Mass::Unit::KG)
-      .value("TON", Mass::Unit::TON)
-      .value("IMP_TON", Mass::Unit::IMP_TON)
-      .value("US_TON", Mass::Unit::US_TON)
-      .value("ST", Mass::Unit::ST)
-      .value("LB", Mass::Unit::LB)
-      .value("OZ", Mass::Unit::OZ);
+        .value("UG", Mass::Unit::UG)
+        .value("MG", Mass::Unit::MG)
+        .value("G", Mass::Unit::G)
+        .value("KG", Mass::Unit::KG)
+        .value("TON", Mass::Unit::TON)
+        .value("IMP_TON", Mass::Unit::IMP_TON)
+        .value("US_TON", Mass::Unit::US_TON)
+        .value("ST", Mass::Unit::ST)
+        .value("LB", Mass::Unit::LB)
+        .value("OZ", Mass::Unit::OZ);
 
     mass_Type
-        .def(py::init<const double, const Mass::Unit>(),
+        
+        .def(
+            py::init<const double, const Mass::Unit>(),
             py::arg("amount") = 0.0,
             py::arg("u") = Mass::Unit::UG)
+
         .def(py::init<const Mass&>())
+
+        .def("__float__",
+            static_cast<double (Mass::*)() const>(&Mass::operator double))
+
         .def("getValue", &Mass::getValue)
+
         .def("getUnit", &Mass::getUnit)
         .def("setUnit", &Mass::setUnit)
+        
         .def("convertTo", &Mass::convertTo)
+        
         .def("toString",
-            static_cast<std::string (Mass::*)() const>(&Mass::toString))
+            static_cast<std::string(Mass::*)() const>(&Mass::toString))
+        
         .def("toString",
-            static_cast<std::string (Mass::*)(const Mass::Unit) const>(&Mass::toString))
-        .def("__float__", static_cast<double (Mass::*)() const>(&Mass::operator double))
-        .def("__str__", static_cast<std::string (Mass::*)() const>(&Mass::toString))
-        .def("__repr__", static_cast<std::string (Mass::*)() const>(&Mass::toString))
-        //.def("assign", &Mass::operator=)
+            static_cast<std::string(Mass::*)(const Mass::Unit) const>(&Mass::toString))
+        
+        .def("__str__",
+            static_cast<std::string(Mass::*)() const>(&Mass::toString))
+        
+        .def_static("convert", &Mass::convert)
+
     ;
 
 
     /**
      * HX711.Value
      */
+
+    /**
+     * std::int32_t is given instead of Value::_INTERNAL_TYPE
+     * which is protected
+     */
     typedef std::int32_t INTERNAL_VALUE_TYPE;
 
-    py::class_<Value> value_Type(m, "Value");
-
-    value_Type
-        .def("isSaturated", &Value::isSaturated)
-        .def("isValid", &Value::isValid)
-
-        /**
-         * std::int32_t is given instead of Value::_INTERNAL_TYPE
-         * which is protected
-         */
-        .def("__int__", static_cast<INTERNAL_VALUE_TYPE (Value::*)() const>(&Value::operator INTERNAL_VALUE_TYPE))
-        //.def("assign", &Value::operator=)
-
-        .def(py::init<const INTERNAL_VALUE_TYPE>())
-        .def(py::init<>())
+    py::class_<Value>(m, "Value")
 
         .def_property_readonly_static(
             "SATURATION_MIN",
@@ -99,117 +145,109 @@ PYBIND11_MODULE(HX711, m) {
         .def_property_readonly_static(
             "SATURATION_MAX",
             [](const py::object&) { return Value::SATURATION_MAX; })
+
+        .def("isSaturated", &Value::isSaturated)
+        .def("isValid", &Value::isValid)
+
+        .def("__int__",
+            static_cast<INTERNAL_VALUE_TYPE(Value::*)() const>(&Value::operator INTERNAL_VALUE_TYPE))
+
+        .def(py::init<const INTERNAL_VALUE_TYPE>())
+        .def(py::init<>())
+
     ;
 
-
-    /**
-     * HX711.Channel
-     */
-    py::enum_<HX711::Channel>(m, "Channel")
-        .value("A", HX711::Channel::A)
-        .value("B", HX711::Channel::B);
-
-
-    /**
-     * HX711.Gain
-     */
-    py::enum_<HX711::Gain>(m, "Gain")
-        .value("GAIN_128", HX711::Gain::GAIN_128)
-        .value("GAIN_32", HX711::Gain::GAIN_32)
-        .value("GAIN_64", HX711::Gain::GAIN_64);
-
-
-    /**
-     * HX711.Rate
-     */
-    py::enum_<HX711::Rate>(m, "Rate")
-        .value("HZ_10", HX711::Rate::HZ_10)
-        .value("HZ_80", HX711::Rate::HZ_80)
-        .value("OTHER", HX711::Rate::OTHER);
-
-
-    /**
-     * HX711.Format
-     */
-    py::enum_<HX711::Format>(m, "Format")
-        .value("MSB", HX711::Format::MSB)
-        .value("LSB", HX711::Format::LSB);
+    //allow for casts to the internal type
+    py::implicitly_convertible<INTERNAL_VALUE_TYPE, Value>();
 
 
     /**
      * HX711.HX711
      */
-    py::class_<HX711::HX711> hx711_Type(m, "HX711");
+    py::class_<HX711::HX711>(m, "HX711")
 
-    hx711_Type
+        .def(py::init<const int, const int, const Rate>())
+
+        .def("begin", &HX711::HX711::begin)
+
         .def("setStrictTiming", &HX711::HX711::setStrictTiming)
         .def("isStrictTiming", &HX711::HX711::isStrictTiming)
+
         .def("getFormat", &HX711::HX711::getFormat)
         .def("setFormat", &HX711::HX711::setFormat)
+
         .def("getDataPin", &HX711::HX711::getDataPin)
         .def("getClockPin", &HX711::HX711::getClockPin)
+
         .def("getChannel", &HX711::HX711::getChannel)
         .def("getGain", &HX711::HX711::getGain)
         .def("setConfig", &HX711::HX711::setConfig)
+
         .def("isReady", &HX711::HX711::isReady)
         .def("readValue", &HX711::HX711::readValue)
+
         .def("powerDown", &HX711::HX711::powerDown)
         .def("powerUp", &HX711::HX711::powerUp)
+
     ;
-
-
-    /**
-     * HX711.StrategyType
-     */
-    py::enum_<HX711::StrategyType>(m, "StrategyType")
-        .value("Samples", StrategyType::Samples)
-        .value("Time", StrategyType::Time);
-
-
-    /**
-     * HX711.ReadType
-     */
-    py::enum_<HX711::ReadType>(m, "ReadType")
-        .value("Median", ReadType::Median)
-        .value("Average",ReadType::Average);
 
 
     /**
      * HX711.Options
      */
-    py::class_<HX711::Options> options_Type(m, "Options");
-    
-    options_Type
+    py::class_<HX711::Options>(m, "Options")
+
         .def_readwrite("stratType", &Options::stratType)
         .def_readwrite("readType", &Options::readType)
         .def_readwrite("samples", &Options::samples)
         .def_readwrite("timeout", &Options::timeout)
+
         .def(py::init<>())
         .def(py::init<const std::size_t>())
         .def(py::init<const std::chrono::nanoseconds>())
+
     ;
 
 
     /**
      * HX711.AbstractScale
      */
-    py::class_<AbstractScale> abstractScale_Type(m, "AbstractScale");
+    py::class_<AbstractScale>(m, "AbstractScale")
 
-    abstractScale_Type
         .def("setUnit", &AbstractScale::setUnit)
         .def("getUnit", &AbstractScale::getUnit)
+
         .def("getReferenceUnit", &AbstractScale::getReferenceUnit)
         .def("setReferenceUnit", &AbstractScale::setReferenceUnit)
+
         .def("getOffset", &AbstractScale::getOffset)
         .def("setOffset", &AbstractScale::setOffset)
+
         .def("normalise", &AbstractScale::normalise)
+        
+        .def("getValues",
+            static_cast<std::vector<Value>(AbstractScale::*)(const std::chrono::nanoseconds)>(&AbstractScale::getValues))
+        
+        .def("getValues",
+            static_cast<std::vector<Value>(AbstractScale::*)(const std::size_t)>(&AbstractScale::getValues))
+        
+        .def("read",
+            &AbstractScale::read,
+            py::arg("o") = Options())
 
-        //getValues virtual functions here
+        .def("zero",
+            &AbstractScale::zero,
+            py::arg("o") = Options())
 
-        .def("read", &AbstractScale::read)
-        .def("zero", &AbstractScale::zero)
+        .def("weight",
+            static_cast<Mass(AbstractScale::*)(const Options)>(&AbstractScale::weight),
+            py::arg("o") = Options())
 
-        //overloaded weight functions here
+        .def("weight",
+            static_cast<Mass(AbstractScale::*)(const std::chrono::nanoseconds)>(&AbstractScale::weight))
+
+        .def("weight",
+            static_cast<Mass(AbstractScale::*)(const std::size_t)>(&AbstractScale::weight))
 
     ;
 
@@ -217,46 +255,26 @@ PYBIND11_MODULE(HX711, m) {
     /**
      * HX711.SimpleHX711
      */
-    py::class_<SimpleHX711, HX711::HX711, AbstractScale> simpleHX711_Type(m, "SimpleHX711");
-
-    simpleHX711_Type
+    py::class_<SimpleHX711, HX711::HX711, AbstractScale>(m, "SimpleHX711")
         .def(py::init<
             const int,
             const int,
             const HX711::Value,
             const HX711::Value,
-            const HX711::Rate>())
-
-        .def(
-            "getValues",
-            static_cast<std::vector<Value> (SimpleHX711::*)(const std::chrono::nanoseconds)>(&SimpleHX711::getValues))
-
-        .def(
-            "getValues",
-            static_cast<std::vector<Value> (SimpleHX711::*)(const std::size_t)>(&SimpleHX711::getValues))
+            const HX711::Rate>()) 
     ;
 
 
     /**
      * HX711.AdvancedHX711
      */
-    py::class_<AdvancedHX711, HX711::HX711, AbstractScale> advancedHX711_Type(m, "AdvancedHX711");
-
-    advancedHX711_Type
+    py::class_<AdvancedHX711, HX711::HX711, AbstractScale>(m, "AdvancedHX711")
         .def(py::init<
             const int,
             const int,
             const HX711::Value,
             const HX711::Value,
             const HX711::Rate>())
-
-        .def(
-            "getValues",
-            static_cast<std::vector<Value> (AdvancedHX711::*)(const std::chrono::nanoseconds)>(&AdvancedHX711::getValues))
-
-        .def(
-            "getValues",
-            static_cast<std::vector<Value> (AdvancedHX711::*)(const std::size_t)>(&AdvancedHX711::getValues))
     ;
 
 }
--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ sys.path.append("../build")
 
 from HX711 import *
 
-hx = SimpleHX711(2, 3, Value(-370), Value(-367471), Rate.HZ_80)
+hx = SimpleHX711(2, 3, -370, -367471, Rate.HZ_80)
 
 while 1:
-	print(hx.normalise(hx.read(Options(3))))
+	print(hx.weight())
