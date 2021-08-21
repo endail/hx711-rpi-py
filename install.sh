2f6d80cf17744bf11138dfebf13b5242d32d0bae
endail <52652357+endail@users.noreply.github.com>
2021-08-22 02:28:20 +1000

    revert

--- install.sh
+++ install.sh
@@ -5,6 +5,14 @@ if [ `id -u` -ne 0 ]; then
     exit 1;
 fi
 
+# build and install liblgpio if not found
+if ! $(ldconfig -p | grep -q liblgpio); then
+    git clone --depth 1 --branch v0.1 https://github.com/joan2937/lg
+    cd lg
+    make && make install
+    cd ..
+fi
+
 # build and install libhx711 if not found
 if ! $(ldconfig -p | grep -q libhx711); then
     git clone https://github.com/endail/hx711
