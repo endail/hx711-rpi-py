e39ebcc3e80f29d5a1a2bb1c920e6cce17d686e7
endail <52652357+endail@users.noreply.github.com>
2021-08-22 01:06:11 +1000

    test

---
+++ install-deps.sh
@@ -0,0 +1,17 @@
+
+git clone https://github.com/joan2937/lg@v0.1
+cd lg
+make
+sudo make install
+
+#sudo ldconfig
+#sudo chown -R $USER:$USER /home/runner/work
+
+git clone https://github.com/endail/hx711
+cd hx711
+make
+sudo make install
+
+#sudo sh -c "echo '/usr/local/lib' >> /etc/ld.so.conf"
+#sudo ldconfig
+#sudo chown -R $USER:$USER /home/runner/work
