3e581a07af77eed6b9a75abcf21e448e76df43a9
endail <52652357+endail@users.noreply.github.com>
2021-08-23 23:14:00 +1000

    test changes

--- install-deps.sh
+++ install-deps.sh
@@ -1,25 +1,12 @@
 
-apt-get update && apt-get install -y --no-install-recommends apt-utils
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt full-upgrade -y
-
+# lgpio
 git clone --depth 1 --branch v0.1 https://github.com/joan2937/lg
 cd lg
 make && make install
 cd ..
 
-#sudo ldconfig
-#sudo chown -R $USER:$USER /home/runner/work
-
+# hx711
 git clone https://github.com/endail/hx711
 cd hx711
 make && make install
 cd ..
-
-#sudo sh -c "echo '/usr/local/lib' >> /etc/ld.so.conf"
-#sudo ldconfig
-#sudo chown -R $USER:$USER /home/runner/work
-
-
-
--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
-# coding: utf-8
 from setuptools import setup, find_packages, Extension
 from pybind11.setup_helpers import Pybind11Extension
 
-__version__ = "0.0.1"
-
 setup(
-    name="HX711",
-    version=__version__,
-    author="Daniel Robertson",
-    author_email="52652357+endail@users.noreply.github.com",
-    url="https://github.com/endail/hx711-rpi-py",
-    description="Python bindings for Raspberry Pi HX711 C++ Library",
-    long_description="",
-    zip_safe=False,
     ext_modules=[
-
         Pybind11Extension(
             "HX711",
             ["src/bindings.cpp"],
@@ -23,17 +11,5 @@ setup(
                 "lgpio"
             ]
         )
-
-#        Extension(
-#            name="HX711",
-#            sources=[
-#                "src/bindings.cpp"
-#            ],
-#            libraries=[
-#                "hx711",
-#                "lgpio"
-#            ]
-#        )
-        
     ]
 )
