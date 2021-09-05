9af11ae1fed773297add8696da1db498f2152893
endail <52652357+endail@users.noreply.github.com>
2021-09-06 01:39:40 +1000

    test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pybind11.setup_helpers import Pybind11Extension
 # https://stackoverflow.com/questions/62362693/how-do-i-read-project-dependencies-from-pyproject-toml-from-my-setup-py-to-avoi
 
 setup(
-    name="HX711",
+    name="hx711-rpi-pi",
     version="0.0.1",
     author="Daniel Robertson",
     author_email="52652357+endail@users.noreply.github.com",
