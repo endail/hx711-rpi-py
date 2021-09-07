3b37028995914e1cd44d8a4196b50679d8b7f8be
endail <52652357+endail@users.noreply.github.com>
2021-09-08 01:36:12 +1000

    test10

--- gh-actions-rpi-cmd.sh
+++ gh-actions-rpi-cmd.sh
@@ -1,10 +1,13 @@
 
-apt-get update -y
-apt-get full-upgrade -y
-apt-get install python3-dev git-all python3-setuptools python3-pip python3-venv -y
+# update os
+apt-get update -y && apt-get full-upgrade -y
 
+# install needed packages
+apt-get install git-all python3-dev python3-setuptools python3-pip python3-venv -y
+
+# install liblgpio and libhx711
 ./install-deps.sh
-apt-get autoremove -y
 
+# build and package
 python3 -m pip install build --user
 python3 -m build --sdist --wheel --outdir dist/ .
--- src/bindings.cpp
+++ src/bindings.cpp
@@ -25,19 +25,10 @@
 #include <string>
 #include <vector>
 #include <hx711/common.h>
-
-#ifdef USE_PYBIND_SUBMODULE
-#include "../extern/pybind11/include/pybind11/chrono.h"
-#include "../extern/pybind11/include/pybind11/operators.h"
-#include "../extern/pybind11/include/pybind11/pybind11.h"
-#include "../extern/pybind11/include/pybind11/stl.h"
-#else
 #include <pybind11/chrono.h>
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#endif
-
 
 using namespace HX711;
 
