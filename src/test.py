7e71c00ba9578f05e933ef8b40da79c5fbce3798
endail <52652357+endail@users.noreply.github.com>
2021-09-11 22:20:41 +1000

    doc update

--- src/test.py
+++ src/test.py
@@ -1,6 +1,5 @@
 from HX711 import SimpleHX711, Rate
 
-hx = SimpleHX711(2, 3, -370, -367471, Rate.HZ_80)
-
-while True:
-	print(hx.weight())
+with SimpleHX711(2, 3, -370, -367471, Rate.HZ_80) as hx:
+    while True:
+        print(hx.weight())
