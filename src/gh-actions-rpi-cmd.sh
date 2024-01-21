9612a51aafba5d3d51f990e389c611b80f8e7d0e
endail <52652357+endail@users.noreply.github.com>
2024-01-21 23:21:18 +1100

    fix missing py pkg

--- src/gh-actions-rpi-cmd.sh
+++ src/gh-actions-rpi-cmd.sh
@@ -4,7 +4,7 @@ export VIRTUAL_PI=1;
 apt-get update -y && apt-get full-upgrade -y;
 
 # install needed packages
-apt-get install git-all python3-dev python3-setuptools python3-pip python3-venv -y;
+apt-get install git-all python3-dev python3-setuptools python3-pip python3-venv python3-build -y;
 
 # install liblgpio and libhx711
 src/install-deps.sh;
